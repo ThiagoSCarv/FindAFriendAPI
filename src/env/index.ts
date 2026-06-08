48ed501628d5a5681a7771b1cc9b4886b34229ac
Thiago Carvalho <thiagocarv4@gmail.com>
2026-06-08 12:06:17 -0300

    feat: add zod env validation schema

---
+++ src/env/index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string().url(),
+  JWT_SECRET: z.string().min(1),
+});
+
+export const env = envSchema.parse(process.env);
