274824901f6200d1c168138af34258047a31cd30
Thiago Carvalho <thiagocarv4@gmail.com>
2026-06-08 13:48:55 -0300

    feat: add PrismaOrgsRepository

---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,16 @@
+import type { Org } from '@prisma/client';
+import { prisma } from '../../lib/prisma.js';
+import type {
+  IOrgsRepository,
+  OrgCreateInput,
+} from '../interfaces/IOrgsRepository.js';
+
+export class PrismaOrgsRepository implements IOrgsRepository {
+  async create(data: OrgCreateInput): Promise<Org> {
+    return prisma.org.create({ data });
+  }
+
+  async findByEmail(email: string): Promise<Org | null> {
+    return prisma.org.findUnique({ where: { email } });
+  }
+}
